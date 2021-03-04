9f196fa11c0ad5fd71791b2ec09976a26409cf94
franktchatseu <louenkamfrank@gmail.com>
2021-03-04 10:03:06 +0100

    starting page template

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  static String _pkg = "gooey_edge";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GooeyEdgeDemo(title: 'Gooey Edge Demo'),
+    );
+  }
+}
+
